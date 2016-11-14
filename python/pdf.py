63ea4797f8f70714c1bd5b4b37a2d76898a6793e
Dominic McDevitt-Parks <mcdevitd@gmail.com>
2016-11-14 17:02:04 -0500

    rework with Wand

--- python/pdf.py
+++ python/pdf.py
@@ -1,49 +1,16 @@
-# This script is adapted from the code at http://nedbatchelder.com/blog/200712/extracting_jpgs_from_pdfs.html
+# This script requires Wand for Python. Install using the documentation at http://docs.wand-py.org/en/0.4.1/index.html before running.
 
-# To use this, simply run the script from the same directory as the PDFs. The script finds all documents in the folder ending in ".pdf" and extracts JPGs from them. This method may only work reliably on scanned documents, which will have a single source image per page.
-
-# The script is currently set up to create a "PDFs" subfolder to save all outputs to.
-
-import sys, os
+import sys, os, datetime
+from wand.image import Image
 
 for item in os.listdir(os.getcwd()):
 	if item.endswith(".pdf"):
-		print item
-
-		pdf = file(item, "rb").read()
-
-		startmark = "\xff\xd8"
-		startfix = 0
-		endmark = "\xff\xd9"
-		endfix = 2
-		i = 0
-		njpg = 1
-
-		while True:
-			istream = pdf.find("stream", i)
-			if istream < 0:
-				break
-			istart = pdf.find(startmark, istream, istream+20)
-			if istart < 0:
-				print pdf
-				i = istream+20
-				continue
-			iend = pdf.find("endstream", istart)
-			if iend < 0:
-				raise Exception("Didn't find end of stream!")
-			iend = pdf.find(endmark, iend-20)
-			if iend < 0:
-				raise Exception("Didn't find end of JPG!")
-
-			istart += startfix
-			iend += endfix
-			print "JPG %d from %d to %d" % (njpg, istart, iend)
-			jpg = pdf[istart:iend]
+		start = datetime.datetime.now()
+		with Image(filename=item) as readpdf:
+			pages = len(readpdf.sequence)
 			if not os.path.exists(os.getcwd() + '/PDFs'):
 				os.makedirs(os.getcwd() + '/PDFs')
-			jpgfile = file(os.getcwd() + "/PDFs/" + item[:-4] + "_(" + str(njpg) + ").jpg", "wb")
-			jpgfile.write(jpg)
-			jpgfile.close()
-	 
-			njpg += 1
-			i = iend
+			jpgfile = os.getcwd() + "/PDFs/" + item[:-4] + ".jpg"
+			readpdf.save(filename=jpgfile)
+		end = datetime.datetime.now()
+		print 'Took ' + str(int(round((end - start).total_seconds()))) + ' seconds for "' + item + '" (' + str(pages) + ' page(s)).'
