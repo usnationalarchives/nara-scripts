4fdb5b2e29f9a59e98ed8771b01f7553871ffab5
Jason Clingerman <jason.clingerman@nara.gov>
2020-01-10 10:26:23 -0500

    Updated rename.py
    Made more comprehensive based on list of special characters that cause issues with AWS S3 and Lambda

--- python/rename.py
+++ python/rename.py
@@ -2,21 +2,42 @@
 
 import os
 
-def rename(folder='J:\\19-0794_US_JULY_PT2'):
-    print 'Renaming files of ' + folder + ' and its subfolders...'
+def rename(folder='objects'):
+    print ('Renaming files of ' + folder + ' and its subfolders...')
     for dirpath, _, filenames in os.walk(folder, topdown=True):
-        print '\nRenaming files from', dirpath, '\n'
+        print ('\nRenaming files from', dirpath, '\n')
         if filenames:
             for filename in filenames:
                 replaced_name = filename.replace(",", "-")
-                replaced_name = replaced_name.replace("~", "-")
+                replaced_name = replaced_name.replace(u"\u2013", "-")
                 replaced_name = replaced_name.replace(";", "-")
+                replaced_name = replaced_name.replace("~", "-")
                 replaced_name = replaced_name.replace("#", "-")
                 replaced_name = replaced_name.replace("*", "-")
-                print filename, "->", replaced_name
+                replaced_name = replaced_name.replace("&", "-")
+                replaced_name = replaced_name.replace("$", "-")
+                replaced_name = replaced_name.replace("@", "-")
+                replaced_name = replaced_name.replace("=", "-")
+                replaced_name = replaced_name.replace(":", "-")
+                replaced_name = replaced_name.replace("+", "-")
+                replaced_name = replaced_name.replace("?", "-")
+                replaced_name = replaced_name.replace("]", "-")
+                replaced_name = replaced_name.replace("[", "-")
+                replaced_name = replaced_name.replace("{", "-")
+                replaced_name = replaced_name.replace("}", "-")
+                replaced_name = replaced_name.replace("%", "-")
+                replaced_name = replaced_name.replace("`", "-")
+                replaced_name = replaced_name.replace("^", "-")
+                replaced_name = replaced_name.replace("<", "-")
+                replaced_name = replaced_name.replace("\"", "-")
+                replaced_name = replaced_name.replace(">", "-")
+                replaced_name = replaced_name.replace("|", "-")
+                replaced_name = replaced_name.replace("\\", "-")              
+                
+                print (filename, "->", replaced_name)
                 os.rename(
                     dirpath + '/' + filename, dirpath + '/' + replaced_name)
-    print '\nAll files renamed!'
+    print ('\nAll files renamed!')
 
 if __name__ == "__main__":
     rename()
