6c3ef41f1f5bc9536239833fbaa3fe3cc385dc62
Jason Clingerman <jason.clingerman@nara.gov>
2020-06-05 07:06:56 -0400

    Update rename.py
    Updates includes additional special characters to replace, and renaming of directories in addition to files.

--- python/rename.py
+++ python/rename.py
@@ -1,43 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import os
-
-def rename(folder='objects'):
-    print ('Renaming files of ' + folder + ' and its subfolders...')
+def renameFiles(folder='specialCharFix'):
+    print 'Renaming files of ' + folder + ' and its subfolders...'
     for dirpath, _, filenames in os.walk(folder, topdown=True):
-        print ('\nRenaming files from', dirpath, '\n')
+        print '\nRenaming files from', dirpath, '\n'
         if filenames:
             for filename in filenames:
                 replaced_name = filename.replace(",", "-")
-                replaced_name = replaced_name.replace(u"\u2013", "-")
-                replaced_name = replaced_name.replace(";", "-")
                 replaced_name = replaced_name.replace("~", "-")
+                replaced_name = replaced_name.replace(";", "-")
                 replaced_name = replaced_name.replace("#", "-")
                 replaced_name = replaced_name.replace("*", "-")
-                replaced_name = replaced_name.replace("&", "-")
-                replaced_name = replaced_name.replace("$", "-")
-                replaced_name = replaced_name.replace("@", "-")
-                replaced_name = replaced_name.replace("=", "-")
-                replaced_name = replaced_name.replace(":", "-")
-                replaced_name = replaced_name.replace("+", "-")
-                replaced_name = replaced_name.replace("?", "-")
-                replaced_name = replaced_name.replace("]", "-")
+                replaced_name = replaced_name.replace("–", "-") #this is the long dash
+                replaced_name = replaced_name.replace("ca.", "ca-")
                 replaced_name = replaced_name.replace("[", "-")
-                replaced_name = replaced_name.replace("{", "-")
-                replaced_name = replaced_name.replace("}", "-")
-                replaced_name = replaced_name.replace("%", "-")
-                replaced_name = replaced_name.replace("`", "-")
-                replaced_name = replaced_name.replace("^", "-")
-                replaced_name = replaced_name.replace("<", "-")
-                replaced_name = replaced_name.replace("\"", "-")
-                replaced_name = replaced_name.replace(">", "-")
-                replaced_name = replaced_name.replace("|", "-")
-                replaced_name = replaced_name.replace("\\", "-")              
-                
-                print (filename, "->", replaced_name)
+                replaced_name = replaced_name.replace("]", "-")
+                replaced_name = replaced_name.replace("(", "-")
+                replaced_name = replaced_name.replace(")", "-")
+                print filename, "->", replaced_name
                 os.rename(
                     dirpath + '/' + filename, dirpath + '/' + replaced_name)
-    print ('\nAll files renamed!')
+    print '\nAll files renamed!'
+
+if __name__ == "__main__":
+    renameFiles()
+
+def rename(folder='specialCharFix'):
+    print 'Renaming folders'
+    for root, dirs, files in os.walk(folder):
+        for directory in dirs:
+            replaced_name = directory.replace("‚Äì", "-")
+            replaced_name = replaced_name.replace("–","-")
+            replaced_name = replaced_name.replace(",", "-")
+            replaced_name = replaced_name.replace("~", "-")
+            replaced_name = replaced_name.replace(";", "-")
+            replaced_name = replaced_name.replace("#", "-")
+            replaced_name = replaced_name.replace("*", "-")
+            replaced_name = replaced_name.replace("ca.", "ca-")
+            replaced_name = replaced_name.replace("[", "-")
+            replaced_name = replaced_name.replace("]", "-")
+            replaced_name = replaced_name.replace("(", "-")
+            replaced_name = replaced_name.replace(")", "-")
+            print(directory, "->"), replaced_name
+            os.rename(
+                root + '/' + directory, root + '/' + replaced_name)
+    print 'All folders renamed!'
+    
 
 if __name__ == "__main__":
     rename()
